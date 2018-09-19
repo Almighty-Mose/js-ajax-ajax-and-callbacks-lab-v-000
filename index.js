1b522cd26d58a37694d30be8778c8e0b972cb9e3
Branden Mosley <branden.mosley1337@gmail.com>
2018-09-19 20:20:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 $(document).ready(function (){
   $.get('https://api.github.com/search/repositories?q=${searchTerms}', function(data) {
     const results = data.items.map(r => 
-    `<img class="avatar" src="${r.owner.avatar_url}" alt="${r.owner.login}'s Avatar Image">`
+    `<img class="avatar" src="${r.owner.avatar_url}" alt="${r.owner.login}'s Avatar Image>
+    <h2><a href=`
     )
     
   $('#results').html(results);
