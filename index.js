d4838d44af4199c658e60360e74ac4fa3cb31bc5
Branden Mosley <branden.mosley1337@gmail.com>
2018-09-19 20:18:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function (){
   $.get('https://api.github.com/search/repositories?q=${searchTerms}', function(data) {
-    const results = data.
+    const results = data.items.map(r => )
     
   $('#results').html(results);
   });
