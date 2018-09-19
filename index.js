1ccbb19e524434620d1035d89ba6e7acb3218073
Branden Mosley <branden.mosley1337@gmail.com>
2018-09-19 20:25:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 $(document).ready(function (){
   $.get('https://api.github.com/search/repositories?q=${searchTerms}', function(data) {
     const results = data.items.map(r => 
-    `<img class="avatar" src="${r.owner.avatar_url}" alt="${r.owner.login}'s Avatar Image>
-    <h2><a href="${r.html_url}">${r.name}</a></h2>
-    <p>${r.description}</p>`
-    );
+    `<div>
+      <img class="avatar" src="${r.owner.avatar_url}" alt="${r.owner.login}'s Avatar Image">
+        <div>
+        <h2><a href="${r.html_url}">${r.name}</a></h2>
+        <p>${r.description} | <a href="#" data-repository="${r.name}" data-owner="${r.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p><a href="${r.owner.html_url}">Owner: ${r.owner.login}</a></p>
+        </div>
+      </div><br></br>`).join("");
     
   $('#results').html(results);
   });
