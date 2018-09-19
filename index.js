7c4f7ec0b98444d993c53f0bcf37334b1d3d65ac
Branden Mosley <branden.mosley1337@gmail.com>
2018-09-19 20:31:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-let searchRepositories = (searchTerms) => {
-$.get('https://api.github.com/search/repositories?q=${searchTerms}', function(data) {
-    const results = data.items.map(r => 
-    `<div>
-      <img class="avatar" src="${r.owner.avatar_url}" alt="${r.owner.login}'s Avatar Image">
-        <div>
-        <h2><a href="${r.html_url}">${r.name}</a></h2>
-        <p>${r.description} | <a href="#" data-repository="${r.name}" data-owner="${r.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-        <p><a href="${r.owner.html_url}">Owner: ${r.owner.login}</a></p>
-        </div>
-      </div><br></br>`).join("");
-    
-    $('#results').html(results);
-  });
-};
+var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
+
+var renderCommit = (commit) => {
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+}
+
+var renderCommits = (data) => {
+  let result = data.map((commit)=>renderCommit(commit)).join('')
+  return `<ul>${result}</ul>`
+}
+
+var showCommits = (el) => {
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+    $('#details').html(renderCommits(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+var renderSearchResult = (result) => {
+  return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+}
+
+var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
+
+var searchRepositories = () => {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      $('#results').html(renderSearchResults(data))
+    }).fail(error => {
+      displayError()
+    })
+}
 
 $(document).ready(function (){
 });
-
-let showCommits = (el) => {
-  
-};
